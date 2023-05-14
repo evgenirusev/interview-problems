7e63cc78154f39e3183e4a4901ca17d1712f8ca2
Evgeni Rusev <evgeni.n.rusev@gmail.com>
2023-05-14 14:07:16 +0300

    formatting update

--- src/bit-manipulation/missing-number.js
+++ src/bit-manipulation/missing-number.js
@@ -1,6 +1,6 @@
 // https://leetcode.com/problems/missing-number
 
-// XOR solution - [O(n) time, O(1) space] 
+// XOR solution - O(n) time, O(1) space
 function missingNumber(nums) {
     let missing = nums.length;
     for (let i = 0; i < nums.length; i++) {
@@ -9,7 +9,7 @@ function missingNumber(nums) {
     return missing;
 }
 
-// Sum solution - [O(n) time O(1) space]
+// Sum solution - O(n) time O(1) space
 function missingNumber(nums) {
     let res = nums.length;
     for (let i = 0; i < nums.length; i++) {
@@ -19,7 +19,7 @@ function missingNumber(nums) {
 }
 
 // Less efficient solutions:
-// Hashing - [O(n) time O(n) space]
-// Brute force - [O(n^2) time O(1) space]
+// Hashing - O(n) time O(n) space
+// Brute force - O(n^2) time O(1) space
 
 module.exports = missingNumber;
